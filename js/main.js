8966f6e84d62a8fb32868fb1af97a8b5870c0185
RomanVinichenko <chicagezzz@gmail.com>
2022-05-21 01:02:10 +0300

    Updates

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $('.logo').click(function () {
-    jQuery('html,body').animate({ scrollTop: 0 }, 800);
+  jQuery('html,body').animate({scrollTop: 0}, 800);
 });
 
 let button = document.querySelector(".pop");
@@ -14,83 +14,97 @@ const prevent = ev => ev.preventDefault();
 
 
 if (button) {
-    button.addEventListener("click", function () {
-        popUp.classList.toggle("popup--active");
-        event.preventDefault();
-        body.style.cssText = `
+  button.addEventListener("click", function () {
+    popUp.classList.toggle("popup--active");
+    event.preventDefault();
+    body.style.cssText = `
         overflow-x: hidden;
         overflow-y: hidden;
         `;
-        document.addEventListener('wheel', prevent, {passive: false});
-    })
+    document.addEventListener('wheel', prevent, {passive: false});
+  })
 }
 
 if (closePopUp) {
-    closePopUp.addEventListener("click", function () {
-        popUp.classList.remove("popup--active");
-        body.style.cssText = `
+  closePopUp.addEventListener("click", function () {
+    popUp.classList.remove("popup--active");
+    body.style.cssText = `
         overflow-x: hidden;
         overflow-y: auto;
         `;
-        document.removeEventListener('wheel', prevent);
-    })
+    document.removeEventListener('wheel', prevent);
+  })
 }
 
 if (closeCookie) {
-    closeCookie.addEventListener("click", function () {
-        cookie.classList.add("cookie--inactive");
-    })
+  closeCookie.addEventListener("click", function () {
+    cookie.classList.add("cookie--inactive");
+  })
 }
 
 let scrollpos = window.scrollY;
 
 
 window.addEventListener('scroll', function () {
-    scrollpos = window.scrollY;
+  scrollpos = window.scrollY;
 
-    if (scrollpos >= 50) {
-        addClass();
-    } else {
-        removeClass();
-    }
+  if (scrollpos >= 50) {
+    addClass();
+  } else {
+    removeClass();
+  }
 });
 
 const addClass = () => {
-    if (header) {
-        header.classList.add('header--active');
-    }
+  if (header) {
+    header.classList.add('header--active');
+  }
 };
 const removeClass = () => {
-    if (header) {
-        header.classList.remove('header--active');
-    }
+  if (header) {
+    header.classList.remove('header--active');
+  }
 };
 
-// const anchors = document.querySelectorAll('a[href*="#"]')
-//
-// for (let anchor of anchors) {
-//     anchor.addEventListener('click', function (e) {
-//         e.preventDefault()
-//
-//         const blockID = anchor.getAttribute('href').substr(1)
-//
-//         document.getElementById(blockID).scrollIntoView({
-//             behavior: 'smooth',
-//             block: 'start'
-//         })
-//     })
-// }
+$('.menu a').on('click', function () {
 
-$('.menu a').on('click', function() {
+  let href = $(this).attr('href');
 
-    let href = $(this).attr('href');
+  $('html, body').animate({
+    scrollTop: $(href).offset().top - 120
+  }, {
+    duration: 370,   // по умолчанию «400»
+    easing: "linear" // по умолчанию «swing»
+  });
 
-    $('html, body').animate({
-        scrollTop: $(href).offset().top -120
-    }, {
-        duration: 370,   // по умолчанию «400»
-        easing: "linear" // по умолчанию «swing»
-    });
+  return false;
+});
 
-    return false;
+$(".get__slider").slick({
+  slidesToShow: 5,
+  infinite: false,
+  slidesToScroll: 1,
+  loop: false,
+  dots: false,
+  arrows: true,
+  // responsive: [{
+  //   breakpoint: 1024,
+  //   settings: {
+  //     slidesToShow: 3,
+  //     infinite: true
+  //   }
+  //
+  // }, {
+  //
+  //   breakpoint: 600,
+  //   settings: {
+  //     slidesToShow: 2,
+  //     dots: true
+  //   }
+  //
+  // },
+  //   {
+  //     breakpoint: 300,
+  //     settings: "unslick" // destroys slick
+  //   }]
 });
