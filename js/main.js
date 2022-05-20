38d29ca57ce6f46f593b0b073570fcd385ce4987
RomanVinichenko <chicagezzz@gmail.com>
2022-05-20 17:17:11 +0300

    Updates

--- js/main.js
+++ js/main.js
@@ -8,6 +8,8 @@ let cookie = document.querySelector(".cookie");
 let closePopUp = document.querySelector(".popup__close");
 let closeCookie = document.querySelector(".cookie__close");
 let body = document.body;
+const header = document.querySelector('.header');
+
 const prevent = ev => ev.preventDefault();
 
 
@@ -39,3 +41,56 @@ if (closeCookie) {
         cookie.classList.add("cookie--inactive");
     })
 }
+
+let scrollpos = window.scrollY;
+
+
+window.addEventListener('scroll', function () {
+    scrollpos = window.scrollY;
+
+    if (scrollpos >= 50) {
+        addClass();
+    } else {
+        removeClass();
+    }
+});
+
+const addClass = () => {
+    if (header) {
+        header.classList.add('header--active');
+    }
+};
+const removeClass = () => {
+    if (header) {
+        header.classList.remove('header--active');
+    }
+};
+
+// const anchors = document.querySelectorAll('a[href*="#"]')
+//
+// for (let anchor of anchors) {
+//     anchor.addEventListener('click', function (e) {
+//         e.preventDefault()
+//
+//         const blockID = anchor.getAttribute('href').substr(1)
+//
+//         document.getElementById(blockID).scrollIntoView({
+//             behavior: 'smooth',
+//             block: 'start'
+//         })
+//     })
+// }
+
+$('.menu a').on('click', function() {
+
+    let href = $(this).attr('href');
+
+    $('html, body').animate({
+        scrollTop: $(href).offset().top -120
+    }, {
+        duration: 370,   // по умолчанию «400»
+        easing: "linear" // по умолчанию «swing»
+    });
+
+    return false;
+});
