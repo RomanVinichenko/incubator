140e1778c67a074aac9a881b6c0ad151719fa30d
RomanVinichenko <chicagezzz@gmail.com>
2022-05-23 15:39:55 +0300

    bugfix

--- app/js/main.js
+++ app/js/main.js
@@ -2,12 +2,11 @@ $(".logo").click(function () {
   jQuery("html,body").animate({scrollTop: 0}, 800);
 });
 
-let button = document.querySelector(".pop");
-let popUp = document.querySelector(".popup");
+// let button = document.querySelector(".pop");
+// let popUp = document.querySelector(".popup");
 let cookie = document.querySelector(".cookie");
-let closePopUp = document.querySelector(".popup__close");
+// let closePopUp = document.querySelector(".popup__close");
 let closeCookie = document.querySelector(".cookie__close");
-let body = document.body;
 const header = document.querySelector(".header");
 const burger = document.querySelector(".menu__burger")
 const menuMobile = document.querySelector(".menu__mobile")
@@ -18,31 +17,31 @@ if (burger) {
   })
 }
 
-const prevent = ev => ev.preventDefault();
+// const prevent = ev => ev.preventDefault();
 
 
-if (button) {
-  button.addEventListener("click", function () {
-    popUp.classList.toggle("popup--active");
-    event.preventDefault();
-    body.style.cssText = `
-        overflow-x: hidden;
-        overflow-y: hidden;
-        `;
-    document.addEventListener("wheel", prevent, {passive: false});
-  })
-}
-
-if (closePopUp) {
-  closePopUp.addEventListener("click", function () {
-    popUp.classList.remove("popup--active");
-    body.style.cssText = `
-        overflow-x: hidden;
-        overflow-y: auto;
-        `;
-    document.removeEventListener("wheel", prevent);
-  })
-}
+// if (button) {
+//   button.addEventListener("click", function () {
+//     popUp.classList.toggle("popup--active");
+//     event.preventDefault();
+//     body.style.cssText = `
+//         overflow-x: hidden;
+//         overflow-y: hidden;
+//         `;
+//     document.addEventListener("wheel", prevent, {passive: false});
+//   })
+// }
+//
+// if (closePopUp) {
+//   closePopUp.addEventListener("click", function () {
+//     popUp.classList.remove("popup--active");
+//     body.style.cssText = `
+//         overflow-x: hidden;
+//         overflow-y: auto;
+//         `;
+//     document.removeEventListener("wheel", prevent);
+//   })
+// }
 
 if (closeCookie) {
   closeCookie.addEventListener("click", function () {
@@ -74,7 +73,7 @@ const removeClass = () => {
   }
 };
 
-$(".menu a").on("click", function () {
+$(".menu a, .mission a").on("click", function () {
 
   let href = $(this).attr("href");
 
@@ -87,29 +86,35 @@ $(".menu a").on("click", function () {
 
   return false;
 });
+$(".menu__list a").on("click", function () {
+  $('.menu__mobile').removeClass('menu__mobile--active');
+});
 
 $(".get__slider").slick({
-  infinite: false,
   slidesToScroll: 1,
   loop: false,
+  infinite: true,
+  slidesToShow: 5,
   dots: false,
   arrows: true,
-  // responsive: [
-  //   {
-  //     breakpoint: 1600,
-  //     settings: {
-  //     }
-  //   },
-    // {
-    //   breakpoint: 600,
-    //   settings: {
-    //     slidesToShow: 2,
-    //     dots: true
-    //   }
-    // },
-    // {
-    //   breakpoint: 300,
-    //   settings: "unslick"
-    // }
-    // ]
+  responsive: [
+    {
+      breakpoint: 999,
+      settings: {
+        slidesToShow: 4,
+      }
+    },
+    {
+      breakpoint: 750,
+      settings: {
+        slidesToShow: 3,
+      }
+    },
+    {
+      breakpoint: 650,
+      settings: {
+        slidesToShow: 2,
+      }
+    }
+  ]
 });
